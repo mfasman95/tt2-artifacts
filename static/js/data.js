02bd426219af9902bb2c5d8e3ec0e5c620d9150e
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2017-11-04 14:22:26 -0400

    weighting bos

--- static/js/data.js
+++ static/js/data.js
@@ -3,7 +3,7 @@ var artifacts = {
         'sort' : 1,
         'name' : 'Book of Shadows',
         'bonus' : ' Prestige Relic',
-        'rating' : 500,
+        'rating' : 1000,
         'ad' : .3,
         'effect' : .05,
 	'gmax' : 0.015,
