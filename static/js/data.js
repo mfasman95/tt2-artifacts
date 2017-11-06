952b8bd5daba591e421cdea7cb106bc0caed9bb1
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2017-11-05 21:59:51 -0500

    update weights

--- static/js/data.js
+++ static/js/data.js
@@ -3,7 +3,7 @@ var artifacts = {
         'sort' : 1,
         'name' : 'Book of Shadows',
         'bonus' : ' Prestige Relic',
-        'rating' : 1000,
+        'rating' : 100,
         'ad' : .3,
         'effect' : .05,
 	'gmax' : 0.015,
