4308ec4282c20934a6643d19203da0f73b1b79d2
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2017-12-19 02:39:13 -0500

    adjustments and balancing

--- static/js/data.js
+++ static/js/data.js
@@ -760,7 +760,7 @@ var artifacts = {
 		'sort' : 37,
 		'name' : 'Laborer\'s Pendant',
 		'bonus' : ' Hand of Midas Gold Bonus',
-		'rating' : 1,
+		'rating' : 1.5,
 		'ad' : .2,
 		'effect' : .1,
 		'max' : -1,
