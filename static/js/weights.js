97692d9d83a0b52772a63cd82b9dbec2e4276d27
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2018-01-24 13:15:51 -0500

    Update weights.js

--- static/js/weights.js
+++ static/js/weights.js
@@ -40,133 +40,133 @@ function adjustWeights() {
 		});
 	}
 	$.each(origWeights, function(k,v) {
-		artifacts[k].rating = v.rating;
+		artifacts.data[k].rating = v.rating;
 	});
 	switch($('#build').val()) {
 		case 'tap':
-			artifacts.dh.rating += 1;
-			artifacts.hb.rating += .5;
-			artifacts.tsos.rating += .5;
-			artifacts.fb.rating += .5;
-			artifacts.cota.rating += .5;
-			artifacts.ttt.rating += .5;
-			artifacts.hh.rating += .25;
-			artifacts.ss.rating += .5;
-			artifacts.tr.rating += .5;
+			artifacts.data.dh.rating += 1;
+			artifacts.data.hb.rating += .5;
+			artifacts.data.tsos.rating += .5;
+			artifacts.data.fb.rating += .5;
+			artifacts.data.cota.rating += .5;
+			artifacts.data.ttt.rating += .5;
+			artifacts.data.hh.rating += .25;
+			artifacts.data.ss.rating += .5;
+			artifacts.data.tr.rating += .5;
 			break;
 		case 'hero':
-			artifacts.hb.rating += 1;
-			artifacts.tsos.rating += 1;
-			artifacts.fb.rating += 1;
-			artifacts.cota.rating += 1;
-			artifacts.ttt.rating += 1;
-			artifacts.hh.rating += .5;
-			artifacts.pof.rating += 1;
+			artifacts.data.hb.rating += 1;
+			artifacts.data.tsos.rating += 1;
+			artifacts.data.fb.rating += 1;
+			artifacts.data.cota.rating += 1;
+			artifacts.data.ttt.rating += 1;
+			artifacts.data.hh.rating += .5;
+			artifacts.data.pof.rating += 1;
 			break;
 		case 'cs':
-			artifacts.hb.rating += 1;
-			artifacts.tsos.rating += 1;
-			artifacts.fb.rating += 1;
-			artifacts.cota.rating += 1;
-			artifacts.coe.rating += .5;
-			artifacts.ttt.rating += 1;
-			artifacts.hh.rating += .5;
-			artifacts.ie.rating += 1;
-			artifacts.orc.rating += 1;
-			artifacts.hos.rating += .5;
-			artifacts.ig.rating += 1.5;
-			artifacts.rt.rating += .5;
-			artifacts.pof.rating += 1;
-			artifacts.as.rating += .5;
-			artifacts.tr.rating += .5;
+			artifacts.data.hb.rating += 1;
+			artifacts.data.tsos.rating += 1;
+			artifacts.data.fb.rating += 1;
+			artifacts.data.cota.rating += 1;
+			artifacts.data.coe.rating += .5;
+			artifacts.data.ttt.rating += 1;
+			artifacts.data.hh.rating += .5;
+			artifacts.data.ie.rating += 1;
+			artifacts.data.orc.rating += 1;
+			artifacts.data.hos.rating += .5;
+			artifacts.data.ig.rating += 1.5;
+			artifacts.data.rt.rating += .5;
+			artifacts.data.pof.rating += 1;
+			artifacts.data.as.rating += .5;
+			artifacts.data.tr.rating += .5;
 			break;
 		case 'hs':
-			artifacts.dh.rating += 1;
-			artifacts.coe.rating += 1;
-			artifacts.ss.rating += .6;
-			artifacts.hb.rating += .5;
-			artifacts.tsos.rating += .5;
-			artifacts.fb.rating += .5;
-			artifacts.cota.rating += .5;
-			artifacts.ttt.rating += .5;
-			artifacts.hh.rating += .25;
-			artifacts.hos.rating += 1;
-			artifacts.ig.rating += 3;
-			artifacts.tm.rating += 1;
-			artifacts.rt.rating += .5;
-			artifacts.bor.rating += 1;
-			artifacts.pof.rating += .5;
-			artifacts.tr.rating += .5;
+			artifacts.data.dh.rating += 1;
+			artifacts.data.coe.rating += 1;
+			artifacts.data.ss.rating += .6;
+			artifacts.data.hb.rating += .5;
+			artifacts.data.tsos.rating += .5;
+			artifacts.data.fb.rating += .5;
+			artifacts.data.cota.rating += .5;
+			artifacts.data.ttt.rating += .5;
+			artifacts.data.hh.rating += .25;
+			artifacts.data.hos.rating += 1;
+			artifacts.data.ig.rating += 3;
+			artifacts.data.tm.rating += 1;
+			artifacts.data.rt.rating += .5;
+			artifacts.data.bor.rating += 1;
+			artifacts.data.pof.rating += .5;
+			artifacts.data.tr.rating += .5;
 			break;
 		case 'sc':
-			artifacts.dh.rating += .6;
-			artifacts.ss.rating += .6;
-			artifacts.hb.rating += .6;
-			artifacts.tsos.rating += .6;
-			artifacts.fb.rating += .6;
-			artifacts.cota.rating += .6;
-			artifacts.ttt.rating += .6;
-			artifacts.hh.rating += .3;
-			artifacts.orc.rating += 1;
-			artifacts.ig.rating += 2.6;
-			artifacts.rt.rating += .5;
-			artifacts.bor.rating += .5;
-			artifacts.pof.rating += .6;
-			artifacts.eoe.rating += 1;
-			artifacts.tr.rating += .5;
-			artifacts.as.rating += .5;
+			artifacts.data.dh.rating += .6;
+			artifacts.data.ss.rating += .6;
+			artifacts.data.hb.rating += .6;
+			artifacts.data.tsos.rating += .6;
+			artifacts.data.fb.rating += .6;
+			artifacts.data.cota.rating += .6;
+			artifacts.data.ttt.rating += .6;
+			artifacts.data.hh.rating += .3;
+			artifacts.data.orc.rating += 1;
+			artifacts.data.ig.rating += 2.6;
+			artifacts.data.rt.rating += .5;
+			artifacts.data.bor.rating += .5;
+			artifacts.data.pof.rating += .6;
+			artifacts.data.eoe.rating += 1;
+			artifacts.data.tr.rating += .5;
+			artifacts.data.as.rating += .5;
 			break;
 		case 'pet':
-			artifacts.foe.rating += 1;
-			artifacts.dh.rating += 1;
-			artifacts.tr.rating += .5;
-			artifacts.coe.rating += 1;
-			artifacts.hos.rating += 1;
-			artifacts.hb.rating += .5;
-			artifacts.tsos.rating += .5;
-			artifacts.fb.rating += .5;
-			artifacts.cota.rating += .5;
-			artifacts.ttt.rating += .5;
-			artifacts.hh.rating += .25;
-			artifacts.orc.rating += 1;
-			artifacts.ig.rating += 1.5;
-			artifacts.rt.rating += .5;
-			artifacts.pof.rating += 1;
-			artifacts.as.rating += .5;
+			artifacts.data.foe.rating += 1;
+			artifacts.data.dh.rating += 1;
+			artifacts.data.tr.rating += .5;
+			artifacts.data.coe.rating += 1;
+			artifacts.data.hos.rating += 1;
+			artifacts.data.hb.rating += .5;
+			artifacts.data.tsos.rating += .5;
+			artifacts.data.fb.rating += .5;
+			artifacts.data.cota.rating += .5;
+			artifacts.data.ttt.rating += .5;
+			artifacts.data.hh.rating += .25;
+			artifacts.data.orc.rating += 1;
+			artifacts.data.ig.rating += 1.5;
+			artifacts.data.rt.rating += .5;
+			artifacts.data.pof.rating += 1;
+			artifacts.data.as.rating += .5;
 			break;
 	}
 	switch($('#hero').val()) {
 		case 'melee':
-			artifacts.tsos.rating += .5;
+			artifacts.data.tsos.rating += .5;
 			break;
 		case 'ranged':
-			artifacts.fb.rating += .5;
+			artifacts.data.fb.rating += .5;
 			break;
 		case 'spell':
-			artifacts.cota.rating += .5;
+			artifacts.data.cota.rating += .5;
 			break;
 	}
 	switch($('#hero2').val()) {
 		case 'ground':
-			artifacts.ttt.rating += .5;
+			artifacts.data.ttt.rating += .5;
 			break;
 		case 'flying':
-			artifacts.hh.rating += 1;
+			artifacts.data.hh.rating += 1;
 			break;
 	}
 	switch($('#chest').val()) {
 		case 'boss':
-			artifacts.hs.rating += .5;
+			artifacts.data.hs.rating += .5;
 			break;
 		case 'chest':
-			artifacts.coc.rating += .5;
+			artifacts.data.coc.rating += .5;
 			break;
 	}
 	switch($('#active').val()) {
 		case 'offline':
-			artifacts.zc.rating += 1;
-			artifacts.af.rating += 1;
+			artifacts.data.zc.rating += 1;
+			artifacts.data.af.rating += 1;
 			break;
 	}
-	artifacts = calculateAll(artifacts, true);
+	artifacts.data = calculateAll(artifacts.data, true);
 }
